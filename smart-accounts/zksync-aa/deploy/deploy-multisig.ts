78a8b42c5c14e4d34efdc0e72d0347459c4aff8f
Sinaver Idrisov <sinavercrimea@gmail.com>
2023-07-10 15:08:53 +0300

    ZkSync PlayFlowMaster (Factory + Account) init changes

---
+++ smart-accounts/zksync-aa/deploy/deploy-multisig.ts
@@ -0,0 +1,139 @@
+import { utils, Wallet, Provider, EIP712Signer, types } from 'zksync-web3';
+import * as ethers from 'ethers';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Get private key from the environment variable
+const PRIVATE_KEY: string = process.env.ZKS_PRIVATE_KEY || '';
+if (!PRIVATE_KEY) {
+  throw new Error('Please set ZKS_PRIVATE_KEY in the environment variables.');
+}
+
+// Put the address of your AA factory
+const AA_FACTORY_ADDRESS = process.env.AA_FACTORY_ADDRESS || '';
+if (!AA_FACTORY_ADDRESS) {
+  throw new Error(
+    'Please set AA_FACTORY_ADDRESS in the environment variables.'
+  );
+}
+
+// Put the address of your Wallet 1
+const MULTISIG_OWNER_ADDRESS_1 = process.env.MULTISIG_OWNER_ADDRESS_1 || '';
+if (!MULTISIG_OWNER_ADDRESS_1) {
+  throw new Error(
+    'Please set MULTISIG_OWNER_ADDRESS_1 in the environment variables.'
+  );
+}
+
+// Put the address of your Wallet 2
+const MULTISIG_OWNER_ADDRESS_2 = process.env.MULTISIG_OWNER_ADDRESS_2 || '';
+if (!MULTISIG_OWNER_ADDRESS_2) {
+  throw new Error(
+    'Please set MULTISIG_OWNER_ADDRESS_2 in the environment variables.'
+  );
+}
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+  const provider = new Provider('https://zksync2-testnet.zksync.dev');
+  const wallet = new Wallet(PRIVATE_KEY).connect(provider);
+  const factoryArtifact = await hre.artifacts.readArtifact(
+    'PayFlowMasterFactory'
+  );
+
+  const aaFactory = new ethers.Contract(
+    AA_FACTORY_ADDRESS,
+    factoryArtifact.abi,
+    wallet
+  );
+
+  // For the simplicity of the tutorial, we will use zero hash as salt
+  const salt =
+    '0x0000000000000000000000000000000000000000000000000000000000000000';
+
+  const tx = await aaFactory.deployAccount(
+    salt,
+    MULTISIG_OWNER_ADDRESS_1,
+    MULTISIG_OWNER_ADDRESS_2
+  );
+  await tx.wait();
+
+  // Getting the address of the deployed contract
+  const abiCoder = new ethers.utils.AbiCoder();
+  const multisigAddress = utils.create2Address(
+    AA_FACTORY_ADDRESS,
+    await aaFactory.aaBytecodeHash(),
+    salt,
+    abiCoder.encode(
+      ['address', 'address'],
+      [MULTISIG_OWNER_ADDRESS_1, MULTISIG_OWNER_ADDRESS_2]
+    )
+  );
+  console.log(`Multisig deployed on address ${multisigAddress}`);
+
+  await hre.run('verify:verify', {
+    address: multisigAddress,
+    contract: 'contracts/PayFlowMaster.sol:PayFlowMaster',
+    constructorArguments: [MULTISIG_OWNER_ADDRESS_1, MULTISIG_OWNER_ADDRESS_2],
+  });
+
+  await (
+    await wallet.sendTransaction({
+      to: multisigAddress,
+      // You can increase the amount of ETH sent to the multisig
+      value: ethers.utils.parseEther('0.008'),
+    })
+  ).wait();
+
+  let aaTx = await aaFactory.populateTransaction.deployAccount(
+    '0x0000000000000000000000000000000000000000000000000000000000000001',
+    Wallet.createRandom().address,
+    Wallet.createRandom().address
+  );
+
+  const gasLimit = await provider.estimateGas(aaTx);
+  const gasPrice = await provider.getGasPrice();
+
+  aaTx = {
+    ...aaTx,
+    from: multisigAddress,
+    gasLimit: gasLimit,
+    gasPrice: gasPrice,
+    chainId: (await provider.getNetwork()).chainId,
+    nonce: await provider.getTransactionCount(multisigAddress),
+    type: 113,
+    customData: {
+      gasPerPubdata: utils.DEFAULT_GAS_PER_PUBDATA_LIMIT,
+    } as types.Eip712Meta,
+    value: ethers.BigNumber.from(0),
+  };
+  const signedTxHash = EIP712Signer.getSignedDigest(aaTx);
+
+  const signature = ethers.utils.concat([
+    // Note, that `signMessage` wouldn't work here, since we don't want
+    // the signed hash to be prefixed with `\x19Ethereum Signed Message:\n`
+    ethers.utils.joinSignature(wallet._signingKey().signDigest(signedTxHash)),
+    ethers.utils.joinSignature(wallet._signingKey().signDigest(signedTxHash)),
+  ]);
+
+  aaTx.customData = {
+    ...aaTx.customData,
+    customSignature: signature,
+  };
+
+  console.log(
+    `The multisig's nonce before the first tx is ${await provider.getTransactionCount(
+      multisigAddress
+    )}`
+  );
+  const sentTx = await provider.sendTransaction(utils.serialize(aaTx));
+  await sentTx.wait();
+
+  // Checking that the nonce for the account has increased
+  console.log(
+    `The multisig's nonce after the first tx is ${await provider.getTransactionCount(
+      multisigAddress
+    )}`
+  );
+}
---
+++ smart-accounts/zksync-aa/hardhat.config.ts
@@ -0,0 +1,40 @@
+import '@matterlabs/hardhat-zksync-deploy';
+import '@matterlabs/hardhat-zksync-solc';
+import '@matterlabs/hardhat-zksync-verify';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+module.exports = {
+  solidity: {
+    version: '0.8.17',
+  },
+  zksolc: {
+    version: 'latest',
+    settings: {
+      isSystem: true,
+    },
+  },
+  defaultNetwork: 'zkSyncTestnet',
+
+  networks: {
+    zkSyncTestnet: {
+      url: 'https://zksync2-testnet.zksync.dev',
+      ethNetwork: 'goerli', // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
+      verifyURL:
+        'https://zksync2-testnet-explorer.zksync.dev/contract_verification',
+      accounts: [process.env.ZKS_PRIVATE_KEY],
+      gas: 2100000,
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      gas: 2100000,
+      accounts: [process.env.ZKS_PRIVATE_KEY],
+      zksync: false,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.GOERLI_ETHERSCAN_API_KEY,
+  },
+};
