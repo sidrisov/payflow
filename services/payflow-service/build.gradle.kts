792b3a879f8fe4b32303791d1972062086ae54d2
Sinaver Idrisov <sinavercrimea@gmail.com>
2023-07-23 14:39:24 +0300

    Added Spring Profiles (local, gcp) running the service in different envs

--- services/payflow-service/build.gradle.kts
+++ services/payflow-service/build.gradle.kts
@@ -1,16 +1,20 @@
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 plugins {
 	application
-	id("org.springframework.boot") version "3.1.1"
+	id("org.springframework.boot") version "3.1.2"
 	id("io.spring.dependency-management") version "1.1.0"
-	//id("com.google.cloud.tools.appengine") version "2.4.5"
+	//id("com.google.cloud.tools.jib") version "3.3.2" - jib
 }
 
 application {
     mainClass.set("ua.sinaver.web3.PayFlowApplication")
 }
 
-extra["springCloudGcpVersion"] = "4.5.1"
-extra["springCloudVersion"] = "2022.0.3"
+if (project.hasProperty("gcp")) {
+	extra["springCloudGcpVersion"] = "4.5.1"
+	extra["springCloudVersion"] = "2022.0.3"
+}
 
 group = "ua.sinaver.web3"
 version = "0.0.1-SNAPSHOT"
@@ -24,13 +28,16 @@ dependencies {
 	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation ("org.springframework.boot:spring-boot-starter-web")
 	
-	// gcp
-	//implementation ("com.google.cloud:spring-cloud-gcp-starter")
-	//implementation ("com.google.cloud:spring-cloud-gcp-starter-sql-mysql")
-	
-	// local
-	//runtimeOnly ("com.h2database:h2")
-  	runtimeOnly ("com.mysql:mysql-connector-j")
+	if (project.hasProperty("gcp")) {
+		project.logger.info("Including GCP dependencies")
+		// gcp
+		implementation ("com.google.cloud:spring-cloud-gcp-starter")
+		implementation ("com.google.cloud:spring-cloud-gcp-starter-sql-mysql")	
+	} else {
+		// local
+		//runtimeOnly ("com.h2database:h2")
+  		runtimeOnly ("com.mysql:mysql-connector-j")
+	}
 
 	// utils
 	implementation("org.apache.commons:commons-lang3:3.12.0")
@@ -41,13 +48,24 @@ dependencies {
 	developmentOnly ("org.springframework.boot:spring-boot-devtools")
 }
 
+
 dependencyManagement {
   imports {
-    mavenBom("com.google.cloud:spring-cloud-gcp-dependencies:${property("springCloudGcpVersion")}")
-    mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	if (project.hasProperty("gcp")) {
+		// gcp
+    	mavenBom("com.google.cloud:spring-cloud-gcp-dependencies:${property("springCloudGcpVersion")}")
+    	mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
   }
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+// gradlew 
+tasks.withType<BootRun> {
+	if (project.hasProperty("gcp")) {
+		systemProperty("spring.profiles.active", "gcp")
+	}	
 }
