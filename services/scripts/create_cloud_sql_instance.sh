07572f3855bc94530b5778b6ea39437720a08898
Sinaver Idrisov <sinavercrimea@gmail.com>
2023-12-04 14:17:23 +0400

    Updated version to v0.0.1-alpha

--- services/scripts/create_cloud_sql_instance.sh
+++ services/scripts/create_cloud_sql_instance.sh
@@ -1,7 +1,10 @@
-gcloud sql instances create payflow-staging-mysql-eu \
+PROJECT_ID=$(gcloud config get-value project)
+gcloud sql instances create ${PROJECT_ID}-mysql-eu \
   --database-version=MYSQL_8_0_35 \
   --region=europe-west4 \
   --cpu=2 \
-  --memory=4G
+  --memory=4G \
+  --enable-bin-log \
+  --backup \
 
-gcloud sql databases create payflow_db --instance=payflow-staging-mysql-eu
+gcloud sql databases create payflow_db --instance=${PROJECT_ID}-mysql-eu
