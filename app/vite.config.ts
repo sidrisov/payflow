c9f84303496274046be3e66ff766b0664b659d54
Sinaver Idrisov <sinavercrimea@gmail.com>
2024-01-16 15:39:21 +0400

    added PWA support

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,43 @@
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
+
 import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ include: '**/*.svg?react' })]
+  plugins: [
+    react(),
+    svgr({ include: '**/*.svg?react' }),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Payflow',
+        short_name: 'Payflow',
+        description: 'Onchain Social Payments',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 });
