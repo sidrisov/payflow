fa051c58dd1f24bf322bb5f487f7a1d6b0de007c
Sinaver Idrisov <sinavercrimea@gmail.com>
2024-04-07 23:13:11 +0400

    added payment cancellaton support

--- app/src/services/payments.ts
+++ app/src/services/payments.ts
@@ -2,11 +2,28 @@ import axios from 'axios';
 import { PaymentType } from '../types/PaymentType';
 import { API_URL } from '../utils/urlConstants';
 
-export async function updatePayment(payment: PaymentType): Promise<boolean | undefined> {
+export async function completePayment(payment: PaymentType): Promise<boolean | undefined> {
   try {
     const response = await axios.put(
       `${API_URL}/api/payment/${payment.referenceId}`,
       { hash: payment.hash },
+      {
+        withCredentials: true
+      }
+    );
+    console.debug(response.status);
+
+    return response.status === 200;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function cancelPayment(payment: PaymentType): Promise<boolean | undefined> {
+  try {
+    const response = await axios.put(
+      `${API_URL}/api/payment/${payment.referenceId}/cancel`,
+      {},
       {
         withCredentials: true
       }
