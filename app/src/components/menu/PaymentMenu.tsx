df3bc2b710c8639e97ccd0d4b10b7263def472c2
Sinaver Idrisov <sinavercrimea@gmail.com>
2024-04-08 00:03:52 +0400

    added source link to payment menu

--- app/src/components/menu/PaymentMenu.tsx
+++ app/src/components/menu/PaymentMenu.tsx
@@ -1,5 +1,5 @@
-import { ListItemIcon, Menu, MenuItem, MenuProps } from '@mui/material';
-import { Cancel } from '@mui/icons-material';
+import { Avatar, Divider, ListItemIcon, Menu, MenuItem, MenuProps } from '@mui/material';
+import { ArrowOutward, Cancel } from '@mui/icons-material';
 import { cancelPayment } from '../../services/payments';
 import { PaymentType } from '../../types/PaymentType';
 import { toast } from 'react-toastify';
@@ -12,6 +12,24 @@ export function PaymentMenu({ payment, ...props }: MenuProps & { payment: Paymen
       style={{ borderRadius: '50px' }}
       transformOrigin={{ horizontal: 'left', vertical: 'top' }}
       anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}>
+      {payment.source && (
+        <MenuItem
+          {...(payment.source.ref && {
+            component: 'a',
+            href: payment.source.ref,
+            target: '_blank'
+          })}>
+          <ListItemIcon>
+            <Avatar
+              src={`/dapps/${payment.source.app.toLowerCase()}.png`}
+              sx={{ width: 20, height: 20 }}
+            />
+          </ListItemIcon>
+          {payment.source.app}
+          <ArrowOutward fontSize="small" sx={{ margin: 1 }} />
+        </MenuItem>
+      )}
+      {payment.source && <Divider />}
       <MenuItem
         onClick={async () => {
           const success = await cancelPayment(payment);
