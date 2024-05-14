1ca1ddb1565f38546eda1eff0d86d7467f852e28
Sinaver Idrisov <sinavercrimea@gmail.com>
2024-05-14 23:04:55 +0400

    removed frames-v2

--- frames-v2/pages/star-wars/@id/+title.ts
+++
@@ -1,7 +0,0 @@
-import type { PageContext } from "vike/types";
-import type { Data } from "./+data";
-
-export function title(pageContext: PageContext<Data>) {
-  const movie = pageContext.data;
-  return movie.title;
-}
--- frames-v2/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import vercel from 'vite-plugin-vercel';
-import vike from 'vike/plugin';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [react(), vike(), vercel({ smart: true })]
-});
