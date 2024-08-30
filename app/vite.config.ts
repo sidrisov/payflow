608b857e8e0d13d153b25f0397c88528c950d4c0
Sinaver Idrisov <sinavercrimea@gmail.com>
2024-08-31 02:57:21 +0300

    fixed the build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { VitePWA } from 'vite-plugin-pwa';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 import { defineConfig, loadEnv } from 'vite';
 
