dff042bd96789a691aa19d70a913d81c1eec89c0
Sinaver Idrisov <sinavercrimea@gmail.com>
2024-08-30 19:06:20 +0300

    updated to get font from google cdn

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,5 @@
 import ReactDOM from 'react-dom/client';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 import 'react-toastify/dist/ReactToastify.css';
 
 import { RouterProvider } from 'react-router-dom';
